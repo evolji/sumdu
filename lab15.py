f7d50d4e5264aa0cbb83dcdef4dac6d3306dc7e7
evolji <76926048+evolji@users.noreply.github.com>
2023-12-21 16:18:27 +0200

    Update lab15.py

--- lab15.py
+++ lab15.py
@@ -22,3 +22,29 @@ aggregated_data = df.groupby("Ключ").agg({"Значення": "count"}).rese
 
 print("\nРезультат агрегації та групування:")
 print(aggregated_data)
+
+
+
+
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+file_path = 'comptagevelo20162.csv'
+data = pd.read_csv(file_path)
+
+data['Date'] = pd.to_datetime(data['Date'], format='%d/%m/%Y', errors='coerce')
+data['Month'] = data['Date'].dt.month
+
+monthly_data = data.groupby('Month')['Berri1'].sum()
+most_popular_month = monthly_data.idxmax()
+print(f"The most popular month for cyclists is {most_popular_month}")
+
+plt.figure(figsize=(10, 6))
+plt.plot(monthly_data.index, monthly_data, marker='o')
+plt.title('Monthly Bicycle Usage')
+plt.xlabel('Month')
+plt.ylabel('Number of Cyclists')
+plt.grid(True)
+plt.show()
